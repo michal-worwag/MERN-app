91b566888f4b30c4a35b3dfa928eafa1186c2f12
Michał Worwąg <michalwo@gmail.com>
2018-10-07 12:14:12 +0200

    Add About, Home and Nav components

---
+++ client/modules/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About!</h2>
+      </div>
+    );
+  }
+}
+
+export default About;
